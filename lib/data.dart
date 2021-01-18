7a52f8f07e1e4b3a3674e0bcbce2f314a3ed7faf
moraesjeremias <jeremias@volanty.com>
2021-01-18 09:28:00 -0300

    create buttons for xylophone

---
+++ lib/data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final buttonPropsList = [
+  {'songIndex': 1, 'color': Colors.red},
+  {'songIndex': 2, 'color': Colors.orange},
+  {'songIndex': 3, 'color': Colors.yellow},
+  {'songIndex': 4, 'color': Colors.green},
+  {'songIndex': 5, 'color': Colors.teal},
+  {'songIndex': 6, 'color': Colors.blue},
+  {'songIndex': 7, 'color': Colors.purple}
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'data.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playAudio(int index) {
+    final audioPlayer = AudioCache();
+    audioPlayer.play('note$index.wav');
+  }
+
+  Widget createFlatButtons(int songIndex, Color buttonColor) {
+    return Expanded(
+      child: FlatButton(
+        color: buttonColor,
+        onPressed: () {
+          playAudio(songIndex);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              createFlatButtons(1, Colors.red),
+              createFlatButtons(2, Colors.orange),
+              createFlatButtons(3, Colors.yellow),
+              createFlatButtons(4, Colors.green),
+              createFlatButtons(5, Colors.teal),
+              createFlatButtons(6, Colors.blue),
+              createFlatButtons(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
